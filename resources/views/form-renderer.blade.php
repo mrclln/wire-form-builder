608ccffd18c2f3e272c5eb72c50b470e6c967966
mrclln <72896606+mrclln@users.noreply.github.com>
2026-08-13 17:58:55 +0800

    Remove hardcoded Submit button from renderer; submission is the consumer's responsibility

--- resources/views/form-renderer.blade.php
+++ resources/views/form-renderer.blade.php
@@ -1,30 +1,31 @@
 @php $steps = $this->steps(); @endphp
 <div x-data="{
-        step: 0,
-        total: {{ count($steps) }},
-        get isWizard() { return this.total > 1; },
-        isFirst() { return this.step === 0; },
-        isLast() { return this.step === this.total - 1; },
-        next() { if (!this.isLast()) this.step++; },
-        prev() { if (!this.isFirst()) this.step--; }
-    }">
+    step: 0,
+    total: {{ count($steps) }},
+    get isWizard() { return this.total > 1; },
+    isFirst() { return this.step === 0; },
+    isLast() { return this.step === this.total - 1; },
+    next() { if (!this.isLast()) this.step++; },
+    prev() { if (!this.isFirst()) this.step--; }
+}">
 
     @if (count($steps) > 1)
         {{-- Wizard progress bar --}}
         <div class="{{ config('wire-form-builder.styles.wizard.progress') }}">
             <template x-for="i in total" :key="i">
                 <span class="{{ config('wire-form-builder.styles.wizard.progress_dot') }}"
-                    :class="i - 1 === step
-                        ? '{{ config('wire-form-builder.styles.wizard.progress_dot_active') }}'
-                        : (i - 1 < step
-                            ? '{{ config('wire-form-builder.styles.wizard.progress_dot_done') }}'
-                            : '')"></span>
+                    :class="i - 1 === step ?
+                        '{{ config('wire-form-builder.styles.wizard.progress_dot_active') }}' :
+                        (i - 1 < step ?
+                            '{{ config('wire-form-builder.styles.wizard.progress_dot_done') }}' :
+                            '')"></span>
             </template>
         </div>
     @endif
 
     @forelse ($steps as $stepIndex => $step)
-        <div x-show="!isWizard || step === {{ $stepIndex }}" @if ($stepIndex > 0) style="display: none;" @endif>
+        <div x-show="!isWizard || step === {{ $stepIndex }}"
+            @if ($stepIndex > 0) style="display: none;" @endif>
             <div class="{{ config('wire-form-builder.styles.form') }}">
                 @if (!is_null($step['section']))
                     @include('wire-form-builder::fields.section', [
@@ -61,8 +62,6 @@
                 class="{{ config('wire-form-builder.styles.wizard.btn_prev') }}">Back</button>
             <button type="button" x-on:click="next()" x-show="!isLast()"
                 class="{{ config('wire-form-builder.styles.wizard.btn_next') }}">Next</button>
-            <button type="button" x-show="isLast()"
-                class="{{ config('wire-form-builder.styles.submit_button') }}">Submit</button>
         </div>
     @endif
 </div>
